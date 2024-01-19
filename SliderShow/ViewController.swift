00a6023d543842ce54d884031a4d614755485104
Anastasiia <nastyak25555@gmail.com>
2024-01-19 19:30:03 +0100

    Initial Commit

---
+++ SliderShow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SliderShow
+//
+//  Created by Анастасия Кутняхова on 19.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
