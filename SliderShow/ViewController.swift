3e691838ddec1295e04b78464a13502139c97393
Anastasiia <nastyak25555@gmail.com>
2024-01-21 15:18:25 +0100

    SliderShow

--- SliderShow/ViewController.swift
+++ SliderShow/ViewController.swift
@@ -8,12 +8,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var viewChangeColour: UIView!
+    
+    @IBOutlet var labelRed: UILabel!
+    @IBOutlet var labelGreen: UILabel!
+    @IBOutlet var labelBlue: UILabel!
+    
+    @IBOutlet var sliderRed: UISlider!
+    @IBOutlet var sliderGreen: UISlider!
+    @IBOutlet var sliderBlue: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        viewChangeColour.layer.cornerRadius = 10
+        
+        // Label
+        labelRed.text = "0.00"
+        labelGreen.text = "0.00"
+        labelBlue.text = "0.00"
+        
+        // Slider
+        sliderRed.minimumTrackTintColor = .red
+        sliderGreen.minimumTrackTintColor = .green
+        sliderBlue.minimumTrackTintColor = .blue
+        
+        // Slider value
+        sliderRed.value = 0
+        sliderRed.minimumValue = 0
+        sliderRed.maximumValue = 1
+        
+        sliderGreen.value = 0
+        sliderGreen.minimumValue = 0
+        sliderGreen.maximumValue = 1
+        
+        sliderBlue.value = 0
+        sliderBlue.minimumValue = 0
+        sliderBlue.maximumValue = 1
     }
-
-
+    
+    
+    func changeColourRed() {
+        viewChangeColour.backgroundColor = UIColor(red: CGFloat(sliderRed.value), green: CGFloat(sliderGreen.value), blue: CGFloat(sliderBlue.value), alpha: 1)
+    }
+    func labelValue() {
+        labelRed.text = String(sliderRed.value)
+        labelGreen.text = String(sliderGreen.value)
+        labelBlue.text = String(sliderBlue.value)
+    }
+    
+    @IBAction func changedViewBaground() {
+        changeColourRed()
+    }
+    @IBAction func changedLabelValue() {
+        labelValue()
+    }
 }
 
